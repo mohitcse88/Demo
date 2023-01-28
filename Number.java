e6068052f5fe6c0e963668b7152c1de48a2c023c
mohitcse88 <mohitkumardoctor@gmail.com>
2023-01-28 23:18:54 +0530

    Lab code testing

---
+++ Number.java
@@ -0,0 +1,15 @@
+// 3 write a program which tells whether a number  is even or odd ?
+
+class Number
+{
+ public static void main(String[]args)
+  {
+ int n = 12 ;
+ if (n%2==0){
+    System.out.println("even");
+    }
+  else{
+ System.out.println("odd");
+}
+}
+}
---
+++ Sum.java
@@ -0,0 +1,13 @@
+class Sum
+{
+ public static void main(String[] args)
+  {
+   int a , b , c , d , e ;
+  a=1;
+  b=2;
+  c=3;
+  d=4;
+  e=a+b+c+d;
+  System.out.println("sum is"+e);
+ }
+}
